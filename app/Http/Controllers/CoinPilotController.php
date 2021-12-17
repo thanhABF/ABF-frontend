c1d72212f93ef0562e85bcaf87a976e329009031
AvanVelzen <50180603+AvanVelzen@users.noreply.github.com>
2021-12-17 15:05:31 +0100

    Update CoinPilotController.php

--- app/Http/Controllers/CoinPilotController.php
+++ app/Http/Controllers/CoinPilotController.php
@@ -192,10 +192,10 @@ class CoinPilotController extends Controller
       $quote = 'BTC';
 
       $invested = PositionsClosed::where([['user_id','=',auth()->user()->id],['pair','LIKE','%'.$quote]])->sum('Invested');
-      $invested = number_format((float)$invested, 8, '.', ' ');
+      $invested = number_format((float)$invested, 8, '.', ',');
 
       $profit = PositionsClosed::where([['user_id','=',auth()->user()->id],['pair','LIKE','%'.$quote]])->sum('NetProfit');
-      $profit = number_format((float)$profit, 8, '.', ' ');
+      $profit = number_format((float)$profit, 8, '.', ',');
 
       $amount_trades = PositionsClosed::where([['user_id','=',auth()->user()->id],['pair','LIKE','%'.$quote]])->count();
 
@@ -261,10 +261,10 @@ class CoinPilotController extends Controller
       $quote = 'USDT';
 
       $invested = PositionsClosed::where([['user_id','=',auth()->user()->id],['pair','LIKE','%'.$quote]])->sum('Invested');
-      $invested = number_format((float)$invested, 2, '.', '');
+      $invested = number_format((float)$invested, 2, '.', ',');
 
       $profit = PositionsClosed::where([['user_id','=',auth()->user()->id],['pair','LIKE','%'.$quote]])->sum('NetProfit');
-      $profit = number_format((float)$profit, 2, '.', '');
+      $profit = number_format((float)$profit, 2, '.', ',');
 
       $amount_trades = PositionsClosed::where([['user_id','=',auth()->user()->id],['pair','LIKE','%'.$quote]])->count();
 
